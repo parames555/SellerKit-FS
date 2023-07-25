4ab32af7819e3e5369168aa65f670897d9a9005a
parames555 <parames55531@gmail.com>
2023-07-25 18:36:35 +0530

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 import 'package:sellerkit/Constant/Configuration.dart';
 import 'package:sellerkit/Controller/ConfigurationController/ConfigurationController.dart';
